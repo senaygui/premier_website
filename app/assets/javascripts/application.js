bd69354913ed205feaf25de840ce1577dfc71044
biruk <abate3010@gmail.com>
2022-02-17 20:38:47 +0300

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,9 @@
 //= require isotope.pkgd.min.js
 //= require venobox.min.js
 //= require aos.js
+//= require slick.min.js
+
+
 
 
 
@@ -312,8 +315,91 @@ $(document).on('turbolinks:load', function(){
       once: true
     });
   }
+  // Preloader js  
   $(window).on('load', function() {
     aos_init();
+  });
+
+  	$(window).on('load', function () {
+		$('.preloader').fadeOut(700);
+  });
+  
+  	// Background-images
+	$('[data-background]').each(function () {
+		$(this).css({
+			'background-image': 'url(' + $(this).data('background') + ')'
+		});
+	});
+
+	//Hero Slider
+	$('.hero-slider').slick({
+		autoplay: true,
+		autoplaySpeed: 7500,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		infinite: true,
+		arrows: true,
+		fade: true,
+		prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
+		nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>',
+		dots: true
+	});
+	$('.hero-slider').slickAnimation();
+
+	// venobox popup
+	$(document).ready(function () {
+		$('.venobox').venobox();
+	});
+
+
+	// filter
+	$(document).ready(function () {
+		var containerEl = document.querySelector('.filtr-container');
+		var filterizd;
+		if (containerEl) {
+			filterizd = $('.filtr-container').filterizr({});
+		}
+		//Active changer
+		$('.filter-controls li').on('click', function () {
+			$('.filter-controls li').removeClass('active');
+			$(this).addClass('active');
+		});
+	});
+
+	//  Count Up
+	function counter() {
+		var oTop;
+		if ($('.count').length !== 0) {
+			oTop = $('.count').offset().top - window.innerHeight;
+		}
+		if ($(window).scrollTop() > oTop) {
+			$('.count').each(function () {
+				var $this = $(this),
+					countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				}, {
+					duration: 1000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function () {
+						$this.text(this.countNum);
+					}
+				});
+			});
+		}
+	}
+	$(window).on('scroll', function () {
+		counter();
+  });
+    $('#slider').slick({
+      dots: true, //Show dot navigation below slides
+      autoplay: true, //auto play
+      autoplaySpeed: 4000, //Playback speed
   });
 })
 // !(function($) {
