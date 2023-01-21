7a6b773c51d49c4d007464b3f4e9a607cc989458
Biruk <abate3010@gmail.com>
2023-01-21 12:26:12 +0300

    update

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 ## application.rb:
 
-module YicWebsite
+module PremierWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
